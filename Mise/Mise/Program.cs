1b473221182e3c846e7bdb15923db2d14bc5be5a
DMzartex <91669692+DMzartex@users.noreply.github.com>
2022-02-17 14:17:53 +0100

    Add files via upload

---
+++ Mise/Mise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Mise
+{
+    class Program
+    {   
+        static void mise(int somme, out int balance, out int balanceActu)
+        {
+            balance = 10000;
+            Console.WriteLine("Entrer votre mise et appuyer sur enter.");
+            somme = int.Parse(Console.ReadLine());
+            balanceActu = balance - somme;
+            balance = balanceActu;
+
+            Console.WriteLine(balance);
+
+        }
+
+        static void Main(string[] args)
+        {
+            int nbrJoueurs;
+            Console.WriteLine("Quel est le nombre de joueur ?");
+            nbrJoueurs = int.Parse(Console.ReadLine());
+
+            // Création de la matrice
+
+            string[,] cartesJoueur = new string[nbrJoueurs, 10];
+        }
+    }
+}
